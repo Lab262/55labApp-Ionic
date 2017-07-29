9f4c492d2c9d00b1f7c769d268276b1dc954bf66
luisresende <luisfrvilela@gmail.com>
2017-07-29 18:33:39 -0300

    implementing form in benefit detail page

--- src/pages/benefit-detail/benefit-detail.ts
+++ src/pages/benefit-detail/benefit-detail.ts
@@ -1,5 +1,6 @@
 import { Component, Input} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'page-benefit-detail',
@@ -9,12 +10,40 @@ export class BenefitDetailPage {
 
   benefit:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public requestBenefitForm:FormGroup;
+
+  placeholderName = "Seu Nome:"
+  placeholderEmail = "Seu email:"
+  placeholderPhoneNumber = "Telefone:"
+  placeholderInterest = "Gostaria de assinar e começar a aproveitar esse benefício..."
+  titleInterest = "Qual é o seu interesse pelo benefício?"
+  titleButtonRequest = "Solicitar"
+
+  //TODO: Change informations from real data
+  dummyName = "Vanessa Raulino"
+  dummyEmail = "vanessa@lab262.com"
+  dummyPhoneNumber = "00000-0000"
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
     this.benefit = navParams.get("benefit");
+    this.buildRequestBenefitForm();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BenefitDetailPage');
+  buildRequestBenefitForm(){
+    this.requestBenefitForm = this.formBuilder.group({
+      name: ['', Validators.compose([Validators.required,
+        Validators.minLength(3)])],
+      email: ['', Validators.compose([Validators.required,
+            Validators.minLength(4)])],
+      phoneNumber: ['', Validators.compose([Validators.minLength(8),
+        Validators.required])],
+      interest: ['', Validators.compose([Validators.required,
+              Validators.minLength(3)])]
+    });
+  }
+
+  requestBenefit(){
+
   }
 
 }
